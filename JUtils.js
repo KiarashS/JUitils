168f7dcce67c1cca97175d3ee9b1c6613f643bde
KiarashS <kiarash.s@hotmail.com>
2022-09-13 20:44:43 +0430

    Update

--- JUtils.js
+++ JUtils.js
@@ -184,3 +184,21 @@ export let mobileReg = /(?=(\d{4})+$)/g;
 
     return canvas.toDataURL("image/png");
 }
+
+
+
+/** Create a hexadecimal color based on a string
+ *
+ * stringToColor("Kiarash Soleimanzadeh"); */
+ export const stringToColor = (str) => {
+    var hash = 0;
+    for (var i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    }
+    var color = '#';
+    for (var i = 0; i < 3; i++) {
+      var value = (hash >> (i * 8)) & 0xFF;
+      color += ('00' + value.toString(16)).substr(-2);
+    }
+    return color;
+  }
